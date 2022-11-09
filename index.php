8a302a75a63969d465c9882f73c1df452bf09d56
Kevin-Chen1 <89753837+Kevin-Chen1@users.noreply.github.com>
2022-11-09 12:09:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
-<?php require_once("header.php"); ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Page Title</title>
-</head>
 <body>
 
+<h2>JavaScript Function </h2>
+
+<p>This function will solve the following 4 * 3</p>
+
+<p id="ligma"></p>
+
 <script>
-document.getElementById("hwk").innerHTML =
+var x = myFunction(4, 3);
+document.getElementById("ligma").innerHTML = x;
+
+function myFunction(a, b) {
+  return a * b;
+}
+</script>
+  
+  <p id="pp" this function will generate random number </p>
+<script>
+document.getElementById("pp").innerHTML =
 Math.floor(Math.random() * 101);
 </script>
   
