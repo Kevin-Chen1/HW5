25a9a44184127dbfd98f459e69a8247f505ae49d
Kevin-Chen1 <89753837+Kevin-Chen1@users.noreply.github.com>
2022-11-09 11:59:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,17 @@
 <body>
 
 <script>
-function myFunc(theObject) {
-  theObject.make = 'Toyota';
+x = findMax(1, 123, 500, 115, 44, 88);
+
+function findMax() {
+  let max = -Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > max) {
+      max = arguments[i];
+    }
+  }
+  return max;
 }
-
-const mycar = {
-  make: 'Honda',
-  model: 'Accord',
-  year: 1998,
-};
-
-// x gets the value "Honda"
-const x = mycar.make;
-
-// the make property is changed by the function
-myFunc(mycar);
-// y gets the value "Toyota"
-const y = mycar.make;
 </script>
   
 
