8edd5456d5d8b8e01588d1169aa4d761fc774ed4
Kevin-Chen1 <89753837+Kevin-Chen1@users.noreply.github.com>
2022-11-09 12:06:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,8 @@
 <body>
 
 <script>
-x = findMax(1, 123, 500, 115, 44, 88);
-
-function findMax() {
-  let max = -Infinity;
-  for (let i = 0; i < arguments.length; i++) {
-    if (arguments[i] > max) {
-      max = arguments[i];
-    }
-  }
-  return max;
-}
+document.getElementById("hwk").innerHTML =
+Math.floor(Math.random() * 101);
 </script>
   
 
